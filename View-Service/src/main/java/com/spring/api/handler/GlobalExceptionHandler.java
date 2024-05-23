9fe23d8489d20f45775463f0d415d13563f29d49
Blanc_et_Noir <jrw9215@gmail.com>
2024-05-24 00:34:09 +0900

    fix: 에러 페이지 버그 수정

--- View-Service/src/main/java/com/spring/api/handler/GlobalExceptionHandler.java
+++ View-Service/src/main/java/com/spring/api/handler/GlobalExceptionHandler.java
@@ -1,39 +1,37 @@
 package com.spring.api.handler;
 
-import org.apache.tomcat.util.json.JSONParser;
-import org.apache.tomcat.util.json.ParseException;
-import org.json.JSONObject;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.RestClientException;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.api.code.Code;
 import com.spring.api.code.DefaultServiceCode;
-import com.spring.api.dto.ResponseDTO;
 import com.spring.api.exception.CustomException;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler({CustomException.class})
-	public ResponseEntity<ErrorResponse> handleCustomException(CustomException e){
-		Code code = e.getCode();		
-		return new ResponseEntity(ResponseDTO.fail(code),code.getStatus());
-	}
-
-	@ExceptionHandler({RestClientException.class})
-	public ResponseEntity<ErrorResponse> handleRestClientException(RestClientException e) throws ParseException {
-		JSONParser parser = new JSONParser(e.getMessage().substring(7, e.getMessage().length()-1));
-		JSONObject obj = (JSONObject)parser.parse();
-		return new ResponseEntity(obj, HttpStatus.valueOf(e.getMessage().substring(0,3)));
+	public ModelAndView handleCustomException(CustomException e){
+		Code code = e.getCode();
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("/error");
+		modelAndView.addObject("code", code.getCode());
+		modelAndView.addObject("message", code.getMessage());
+		modelAndView.addObject("status", code.getStatus());
+		
+		return modelAndView;
 	}
 	
 	@ExceptionHandler({Exception.class})
-	public ResponseEntity<ErrorResponse> handleException(Exception e) {
-		ResponseDTO dto = ResponseDTO.fail(DefaultServiceCode.INTERNAL_SERVER_ERROR);
-		return new ResponseEntity(dto, dto.getCode().getStatus());
+	public ModelAndView handleException(Exception e) {
+		Code code = DefaultServiceCode.INTERNAL_SERVER_ERROR;
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("/error");
+		modelAndView.addObject("code", code.getCode());
+		modelAndView.addObject("message", code.getMessage());
+		modelAndView.addObject("status", code.getStatus());
+		
+		return modelAndView;
 	}
 }
